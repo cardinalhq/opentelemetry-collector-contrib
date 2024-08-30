c14c36874748681124590f381f1ddb5534c918ef
Michael Graff <mgraff@cardinalhq.io>
2024-08-30 01:10:07 -0500

    handle old format config with warnings

--- receiver/awsfirehosereceiver/config.go
+++ receiver/awsfirehosereceiver/config.go
@@ -21,6 +21,18 @@ type Config struct {
 	AccessKey configopaque.String `mapstructure:"access_key"`
 	Metrics   []MetricsConfig     `mapstructure:"metrics"`
 	Logs      []LogsConfig        `mapstructure:"logs"`
+
+	// Deprecated: Use Metrics instead.
+
+	// NamePrefixes is a list of attributes that are used to determine
+	// the name of the metric. If the attribute is not found in the
+	// record, or its string value is empty, the default value is used.
+	// Fields are applied in order, and sepatated by a period.
+	// Currently, only resource attributes are supported.
+	NamePrefixes []NamePrefixConfig `mapstructure:"name_prefixes"`
+	// RecordType is the key used to determine which unmarshaler to use
+	// when receiving the requests.
+	RecordType string `mapstructure:"record_type"`
 }
 
 type MetricsConfig struct {
