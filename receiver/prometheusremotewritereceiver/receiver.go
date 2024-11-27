de7fdac467877176368676da90d347005104f361
Michael Graff <mgraff@cardinalhq.io>
2024-11-27 10:18:37 -0600

    start to handle v1 proto

--- receiver/prometheusremotewritereceiver/receiver.go
+++ receiver/prometheusremotewritereceiver/receiver.go
@@ -14,12 +14,11 @@ import (
 
 	"github.com/gogo/protobuf/proto"
 	promconfig "github.com/prometheus/prometheus/config"
+	writev1 "github.com/prometheus/prometheus/prompb"
 	writev2 "github.com/prometheus/prometheus/prompb/io/prometheus/write/v2"
-	promremote "github.com/prometheus/prometheus/storage/remote"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/component/componentstatus"
 	"go.opentelemetry.io/collector/consumer"
-	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/receiver"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -95,15 +94,49 @@ func (prw *prometheusRemoteWriteReceiver) handlePRW(w http.ResponseWriter, req *
 		return
 	}
 
-	if msgType != promconfig.RemoteWriteProtoMsgV2 {
+	switch msgType {
+	case promconfig.RemoteWriteProtoMsgV1:
+		prw.handlePRWV1(w, req)
+	case promconfig.RemoteWriteProtoMsgV2:
+		prw.handlePRWV2(w, req)
+	default:
+
 		prw.settings.Logger.Warn("message received with unsupported proto version, rejecting", zap.String("contentType", contentType), zap.String("protoVersion", protoVersion))
 		http.Error(w, "Unsupported proto version", http.StatusUnsupportedMediaType)
 		return
 	}
+}
 
-	// After parsing the content-type header, the next step would be to handle content-encoding.
-	// Luckly confighttp's Server has middleware that already decompress the request body for us.
+func (prw *prometheusRemoteWriteReceiver) handlePRWV1(w http.ResponseWriter, req *http.Request) {
+	body, err := io.ReadAll(req.Body)
+	if err != nil {
+		prw.settings.Logger.Warn("Error decoding remote write request", zapcore.Field{Key: "error", Type: zapcore.ErrorType, Interface: err})
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
 
+	var prw1Req writev1.WriteRequest
+	if err = proto.Unmarshal(body, &prw1Req); err != nil {
+		prw.settings.Logger.Warn("Error decoding remote write request", zapcore.Field{Key: "error", Type: zapcore.ErrorType, Interface: err})
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	m, stats, err := prw.translateV1(req.Context(), &prw1Req)
+	stats.SetHeaders(w)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest) // Following instructions at https://prometheus.io/docs/instrumenting/exposition_formats/#text-based-format
+		return
+	}
+
+	if m.DataPointCount() > 0 {
+		prw.nextConsumer.ConsumeMetrics(req.Context(), m)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (prw *prometheusRemoteWriteReceiver) handlePRWV2(w http.ResponseWriter, req *http.Request) {
 	body, err := io.ReadAll(req.Body)
 	if err != nil {
 		prw.settings.Logger.Warn("Error decoding remote write request", zapcore.Field{Key: "error", Type: zapcore.ErrorType, Interface: err})
@@ -149,84 +182,4 @@ func (prw *prometheusRemoteWriteReceiver) parseProto(contentType string, protoVe
 
 	// assume v1.
 	return promconfig.RemoteWriteProtoMsgV1, nil
-}
-
-// translateV2 translates a v2 remote-write request into OTLP metrics.
-// For now translateV2 is not implemented and returns an empty metrics.
-// nolint
-func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *writev2.Request) (pmetric.Metrics, promremote.WriteResponseStats, error) {
-	stats := promremote.WriteResponseStats{}
-	if v2r == nil {
-		return pmetric.NewMetrics(), stats, errors.New("empty request")
-	}
-
-	for idx, sym := range v2r.Symbols {
-		prw.settings.Logger.Info("Symbol", zap.Int("index", idx), zap.String("symbol", sym))
-	}
-
-	for _, ts := range v2r.Timeseries {
-		labels := derefLabels(ts.LabelsRefs, v2r.Symbols)
-		prw.settings.Logger.Info("Timeseries",
-			zap.Any("labels", labels),
-			zap.Any("samples", ts.Samples),
-			zap.Any("exemplars", ts.Exemplars),
-			zap.Any("histograms", ts.Histograms))
-
-		switch ts.Metadata.Type {
-		case writev2.Metadata_METRIC_TYPE_COUNTER:
-			stats.Samples += len(ts.Samples)
-		case writev2.Metadata_METRIC_TYPE_GAUGE:
-			stats.Samples += len(ts.Samples)
-		case writev2.Metadata_METRIC_TYPE_SUMMARY:
-			if len(ts.Histograms) > 0 {
-				stats.Samples += len(ts.Histograms)
-			} else {
-				stats.Samples += len(ts.Samples)
-			}
-		case writev2.Metadata_METRIC_TYPE_HISTOGRAM:
-			if len(ts.Histograms) > 0 {
-				stats.Samples += len(ts.Histograms)
-			} else {
-				stats.Samples += len(ts.Samples)
-			}
-		case writev2.Metadata_METRIC_TYPE_GAUGEHISTOGRAM:
-			if len(ts.Histograms) > 0 {
-				stats.Samples += len(ts.Histograms)
-			} else {
-				stats.Samples += len(ts.Samples)
-			}
-		default:
-			prw.settings.Logger.Warn("Unknown metric type", zap.Any("type", ts.Metadata.Type))
-		}
-		stats.Exemplars += len(ts.Exemplars)
-	}
-	return pmetric.NewMetrics(), stats, nil
-}
-
-func safeSymbol(symbols []string, index uint32) string {
-	if int(index) < len(symbols) {
-		return symbols[index]
-	}
-	return ""
-}
-
-func derefLabels(labelsRefs []uint32, symbols []string) map[string]string {
-	labels := map[string]string{}
-
-	// Ensure labelsRefs has an even number of entries
-	if len(labelsRefs)%2 != 0 {
-		return labels
-	}
-
-	for i := 0; i < len(labelsRefs); i += 2 {
-		key := safeSymbol(symbols, labelsRefs[i])
-		value := safeSymbol(symbols, labelsRefs[i+1])
-
-		if key == "" || value == "" {
-			continue
-		}
-
-		labels[key] = value
-	}
-	return labels
 }
