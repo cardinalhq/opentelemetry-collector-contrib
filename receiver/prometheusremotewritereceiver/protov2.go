b11b1bdb013b93738ffc6eaf23e04de3206b48ff
Michael Graff <explorer@flame.org>
2024-11-27 12:13:25 -0600

    add handling for counters and gauges

--- receiver/prometheusremotewritereceiver/protov2.go
+++ receiver/prometheusremotewritereceiver/protov2.go
@@ -6,9 +6,11 @@ package prometheusremotewritereceiver // import "github.com/open-telemetry/opent
 import (
 	"context"
 	"errors"
+	"time"
 
 	writev2 "github.com/prometheus/prometheus/prompb/io/prometheus/write/v2"
 	promremote "github.com/prometheus/prometheus/storage/remote"
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.uber.org/zap"
 )
@@ -26,18 +28,43 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 		prw.settings.Logger.Info("Symbol", zap.Int("index", idx), zap.String("symbol", sym))
 	}
 
+	m := pmetric.NewMetrics()
+
 	for _, ts := range v2r.Timeseries {
 		labels := derefLabels(ts.LabelsRefs, v2r.Symbols)
-		prw.settings.Logger.Info("Timeseries",
-			zap.Any("labels", labels),
-			zap.Any("samples", ts.Samples),
-			zap.Any("exemplars", ts.Exemplars),
-			zap.Any("histograms", ts.Histograms))
+		name := labels["__name__"]
+		if name == "" {
+			prw.settings.Logger.Warn("Missing metric name")
+			continue
+		}
+		delete(labels, "__name__")
 
 		switch ts.Metadata.Type {
 		case writev2.Metadata_METRIC_TYPE_COUNTER:
+			rm := m.ResourceMetrics().AppendEmpty()
+			sm := rm.ScopeMetrics().AppendEmpty()
+			m := sm.Metrics().AppendEmpty()
+			sum := m.SetEmptySum()
+			sum.SetIsMonotonic(true)
+			sum.SetAggregationTemporality(pmetric.AggregationTemporalityCumulative)
+			m.SetName(name)
+			for _, sample := range ts.Samples {
+				dp := sum.DataPoints().AppendEmpty()
+				dp.SetTimestamp(pcommon.NewTimestampFromTime(time.UnixMilli(sample.Timestamp)))
+				dp.SetDoubleValue(sample.Value)
+			}
 			stats.Samples += len(ts.Samples)
 		case writev2.Metadata_METRIC_TYPE_GAUGE:
+			rm := m.ResourceMetrics().AppendEmpty()
+			sm := rm.ScopeMetrics().AppendEmpty()
+			m := sm.Metrics().AppendEmpty()
+			g := m.SetEmptyGauge()
+			m.SetName(name)
+			for _, sample := range ts.Samples {
+				dp := g.DataPoints().AppendEmpty()
+				dp.SetTimestamp(pcommon.NewTimestampFromTime(time.UnixMilli(sample.Timestamp)))
+				dp.SetDoubleValue(sample.Value)
+			}
 			stats.Samples += len(ts.Samples)
 		case writev2.Metadata_METRIC_TYPE_SUMMARY:
 			if len(ts.Histograms) > 0 {
@@ -57,12 +84,14 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 			} else {
 				stats.Samples += len(ts.Samples)
 			}
+		case writev2.Metadata_METRIC_TYPE_UNSPECIFIED:
+			// ignore
 		default:
 			prw.settings.Logger.Warn("Unknown metric type", zap.Any("type", ts.Metadata.Type))
 		}
 		stats.Exemplars += len(ts.Exemplars)
 	}
-	return pmetric.NewMetrics(), stats, nil
+	return m, stats, nil
 }
 
 func safeSymbol(symbols []string, index uint32) string {
