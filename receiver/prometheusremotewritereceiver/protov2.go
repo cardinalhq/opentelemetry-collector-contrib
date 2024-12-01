311da71f0df537d8086f3867e49de069d7c9254f
ruchirj <ruchir.jha@gmail.com>
2024-12-01 11:13:38 -0800

    Removing merger.Merge in favor of simple map based merging to potentially solve for memory issues

--- receiver/prometheusremotewritereceiver/protov2.go
+++ receiver/prometheusremotewritereceiver/protov2.go
@@ -6,10 +6,11 @@ package prometheusremotewritereceiver // import "github.com/open-telemetry/opent
 import (
 	"context"
 	"errors"
+	"fmt"
+	"sort"
 	"strings"
 	"time"
 
-	merger "github.com/open-telemetry/opentelemetry-collector-contrib/internal/exp/metrics"
 	writev2 "github.com/prometheus/prometheus/prompb/io/prometheus/write/v2"
 	promremote "github.com/prometheus/prometheus/storage/remote"
 	"go.opentelemetry.io/collector/pdata/pcommon"
@@ -17,6 +18,14 @@ import (
 	semconv "go.opentelemetry.io/collector/semconv/v1.27.0"
 	"go.uber.org/zap"
 )
+
+var resourceAttributeNameMap = map[string]string{
+	"service":   semconv.AttributeServiceName,
+	"job":       semconv.AttributeServiceName,
+	"namespace": semconv.AttributeServiceNamespace,
+	"pod":       semconv.AttributeK8SPodName,
+	"node":      semconv.AttributeK8SNodeName,
+}
 
 // translateV2 translates a v2 remote-write request into OTLP metrics.
 // For now translateV2 is not implemented and returns an empty metrics.
@@ -31,6 +40,8 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 	ignoredHistograms := 0
 
 	metrics := pmetric.NewMetrics()
+	resourceMetricCache := make(map[string]pmetric.ResourceMetrics)
+	scopeMetricCache := make(map[string]pmetric.ScopeMetrics)
 
 	for _, ts := range v2r.Timeseries {
 		labels := derefLabels(ts.LabelsRefs, v2r.Symbols)
@@ -42,10 +53,7 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 		name = strings.ReplaceAll(name, "_", ".")
 		delete(labels, "..name..")
 
-		newMetrics := pmetric.NewMetrics()
-		rm := newMetrics.ResourceMetrics().AppendEmpty()
-		sm := rm.ScopeMetrics().AppendEmpty()
-		m := sm.Metrics().AppendEmpty()
+		m := getMetric(labels, metrics, resourceMetricCache, scopeMetricCache)
 		m.SetName(name)
 
 		units := safeSymbol(v2r.Symbols, ts.Metadata.UnitRef)
@@ -67,7 +75,7 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 				dp := sum.DataPoints().AppendEmpty()
 				dp.SetTimestamp(pcommon.NewTimestampFromTime(time.UnixMilli(sample.Timestamp)))
 				dp.SetDoubleValue(sample.Value)
-				setAttributes(labels, rm.Resource().Attributes(), sm.Scope().Attributes(), dp.Attributes())
+				setAttributes(labels, dp.Attributes())
 			}
 			stats.Samples += len(ts.Samples)
 		case writev2.Metadata_METRIC_TYPE_GAUGE:
@@ -76,7 +84,7 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 				dp := gauge.DataPoints().AppendEmpty()
 				dp.SetTimestamp(pcommon.NewTimestampFromTime(time.UnixMilli(sample.Timestamp)))
 				dp.SetDoubleValue(sample.Value)
-				setAttributes(labels, rm.Resource().Attributes(), sm.Scope().Attributes(), dp.Attributes())
+				setAttributes(labels, dp.Attributes())
 			}
 			stats.Samples += len(ts.Samples)
 		case writev2.Metadata_METRIC_TYPE_SUMMARY:
@@ -110,9 +118,6 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 			prw.settings.Logger.Warn("Unknown metric type", zap.Any("type", ts.Metadata.Type))
 		}
 		stats.Exemplars += len(ts.Exemplars)
-
-		// Merge the new metrics into the existing metrics
-		metrics = merger.Merge(metrics, newMetrics)
 	}
 
 	if ignoredSamples > 0 || ignoredHistograms > 0 {
@@ -122,18 +127,9 @@ func (prw *prometheusRemoteWriteReceiver) translateV2(_ context.Context, v2r *wr
 	return metrics, stats, nil
 }
 
-func setAttributes(labels map[string]string, rattrs pcommon.Map, _ pcommon.Map, iattr pcommon.Map) {
+func setAttributes(labels map[string]string, iattr pcommon.Map) {
 	for k, v := range labels {
-		switch k {
-		case "service":
-			rattrs.PutStr(semconv.AttributeServiceName, v)
-		case "namespace":
-			rattrs.PutStr(semconv.AttributeServiceNamespace, v)
-		case "pod":
-			rattrs.PutStr(semconv.AttributeK8SPodName, v)
-		case "node":
-			rattrs.PutStr(semconv.AttributeK8SNodeName, v)
-		default:
+		if _, ok := resourceAttributeNameMap[k]; !ok {
 			iattr.PutStr(k, v)
 		}
 	}
@@ -163,4 +159,46 @@ func derefLabels(labelsRefs []uint32, symbols []string) map[string]string {
 		labels[key] = value
 	}
 	return labels
+}
+
+func getMetric(
+	labels map[string]string,
+	metrics pmetric.Metrics,
+	resourceMetricCache map[string]pmetric.ResourceMetrics,
+	scopeMetricCache map[string]pmetric.ScopeMetrics,
+) pmetric.Metric {
+
+	var parts []string
+	resourceAttributes := make(map[string]string)
+	for promName, semanticName := range resourceAttributeNameMap {
+		if value, ok := labels[promName]; ok {
+			resourceAttributes[semanticName] = value
+			parts = append(parts, fmt.Sprintf("%s=%s", semanticName, value))
+		}
+	}
+	sort.Strings(parts)
+	resourceID := strings.Join(parts, ":")
+
+	var rm pmetric.ResourceMetrics
+	if cachedRM, ok := resourceMetricCache[resourceID]; ok {
+		rm = cachedRM
+	} else {
+		rm = metrics.ResourceMetrics().AppendEmpty()
+		for k, v := range resourceAttributes {
+			rm.Resource().Attributes().PutStr(k, v)
+		}
+		resourceMetricCache[resourceID] = rm
+	}
+
+	// We index ScopeMetrics with the resourceID itself because there are no discernible scope attributes
+	// that we know of coming on the prometheus remote write request.
+	var sm pmetric.ScopeMetrics
+	if cachedSM, ok := scopeMetricCache[resourceID]; ok {
+		sm = cachedSM
+	} else {
+		sm = rm.ScopeMetrics().AppendEmpty()
+		scopeMetricCache[resourceID] = sm
+	}
+
+	return sm.Metrics().AppendEmpty()
 }
